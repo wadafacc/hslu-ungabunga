d3a3fad14af0c121a308992eb4884839cc89d4fb
wadafacc <schlumpftimo14@gmail.com>
2025-11-23 16:00:17 +0100

    rdztfzugvgctdrz5f6rut7

---
+++ ADS/uebung03/al/aufgabe02/no_such_node_exception.py
@@ -0,0 +1,11 @@
+
+# HSLU / ICS/AIML : Modul ADS : Algorithmen & Datenstrukturen
+# Path   : uebung03/al/aufgabe02
+# Version: Mon Sep 29 11:00:44 CEST 2025
+
+class NoSuchNodeException(Exception):
+
+    def __init__(self, err):
+      super().__init__(err)
+      
+
